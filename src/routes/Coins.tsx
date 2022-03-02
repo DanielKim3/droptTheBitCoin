f6f8839e86483093ca6e1c71238adec9116a68a5
kimwoojin <wjinkim21@naver.com>
2022-03-02 21:19:36 +0900

    추가 : 라우터

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components"
+
+const Title = styled.h1`
+    color:${props => props.theme.accentColor};
+`
+
+function Coins (){ 
+    return <Title>코인</Title>
+}
+export default Coins;
